4cc07d69fe87a70d713371361ce601c13e3582dc
William Antonio <william.fatecsjc@gmail.com>
2016-11-11 21:24:54 -0200

    Adding JavaFX integration

--- hello/src/main/java/org/jbugkorea/hello/Hello.java
+++ hello/src/main/java/org/jbugkorea/hello/Hello.java
@@ -1,7 +1,25 @@
 package org.jbugkorea.hello;
 
-public class Hello {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Hello extends Application {
+	
     public void say() {
-        System.out.println("Hello!");
+        launch();
     }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Label lblHello = new Label("Hello World!");
+		StackPane root = new StackPane(lblHello);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setWidth(300);
+		stage.setHeight(300);
+		stage.show();
+	}
 }
---
+++ mods/javafx/main/README.md
@@ -0,0 +1,1 @@
+ Copy jfxrt.jar from your Java installation here. Due to licensing issues, I can't redistribute it.
